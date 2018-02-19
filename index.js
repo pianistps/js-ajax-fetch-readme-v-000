e8d6b64f8ef0246addac39a75bada0fafd26e8c9
github username pianistps <pianistps@yahoo.com>
2018-02-19 14:43:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'ed028c58b7133bf279c5d53086f9e45c10e691de'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
